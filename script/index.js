1d0d1269a5ed3a37ef223c0520b85ddc198bd8f6
i-webtest <stasbass80@gmail.com>
2022-12-27 20:07:11 +0300

    Get 4 elements from the modal window

---
+++ script/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const modalTitle = modal.querySelector('.modal__title');
+const modalCheckbox = modal.querySelector('.modal__checkbox');
+const modalInputDiscount = modal.querySelector('.modal__input_discount');

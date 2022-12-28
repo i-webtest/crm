c122f78801a40596cf14784e35ee88b7ca701b84
i-webtest <stasbass80@gmail.com>
2022-12-28 16:58:44 +0300

    The active class was removed from the overlay class element

--- script/index.js
+++ script/index.js
@@ -1,11 +1,10 @@
-"use strict";
+'use strict';
 
-const modal = document.querySelector(".modal");
-const modalTitle = modal.querySelector(".modal__title");
-const modalCheckbox = modal.querySelector(".modal__checkbox");
-const modalInputDiscount = modal.querySelector(".modal__input_discount");
-const overlay = document.querySelector(".overlay");
+const modal = document.querySelector('.modal');
+const modalTitle = modal.querySelector('.modal__title');
+const modalCheckbox = modal.querySelector('.modal__checkbox');
+const modalInputDiscount = modal.querySelector('.modal__input_discount');
+const overlay = document.querySelector('.overlay');
+const tableBody = document.querySelector('.table__body');
 
-overlay.classList.remove("active");
-
-const createRow = (obj) => {};
+overlay.classList.remove('active');

d163e43dce349575f88be5349ccfd88167d0546c
i-webtest <stasbass80@gmail.com>
2022-12-28 11:17:20 +0300

    Created new branch lesson_03

--- script/index.js
+++ script/index.js
@@ -1,6 +1,11 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal');
-const modalTitle = modal.querySelector('.modal__title');
-const modalCheckbox = modal.querySelector('.modal__checkbox');
-const modalInputDiscount = modal.querySelector('.modal__input_discount');
+const modal = document.querySelector(".modal");
+const modalTitle = modal.querySelector(".modal__title");
+const modalCheckbox = modal.querySelector(".modal__checkbox");
+const modalInputDiscount = modal.querySelector(".modal__input_discount");
+const overlay = document.querySelector(".overlay");
+
+overlay.classList.remove("active");
+
+const createRow = (obj) => {};

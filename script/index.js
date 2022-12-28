8a7434403957872b317cc887c9e1c11d5121e1ba
i-webtest <stasbass80@gmail.com>
2022-12-28 17:00:14 +0300

    Created the createRow function, which receives an object and forms an element

--- script/index.js
+++ script/index.js
@@ -8,3 +8,32 @@ const overlay = document.querySelector('.overlay');
 const tableBody = document.querySelector('.table__body');
 
 overlay.classList.remove('active');
+
+const createRow = (goods) => {
+  const tr = document.createElement('tr');
+
+  tr.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <td class="table__cell"></td>
+    <td class="table__cell table__cell_left table__cell_name" data-id="${
+      goods.id
+    }">
+      <span class="table__cell-id">id: ${goods.id}</span>
+      ${goods.title}
+    </td>
+    <td class="table__cell table__cell_left">${goods.category}</td>
+    <td class="table__cell">${goods.units}</td>
+    <td class="table__cell">${goods.count}</td>
+    <td class="table__cell">$${goods.price}</td>
+    <td class="table__cell">$${goods.count * goods.price}</td>
+    <td class="table__cell table__cell_btn-wrapper">
+      <button class="table__btn table__btn_pic"></button>
+      <button class="table__btn table__btn_edit"></button>
+      <button class="table__btn table__btn_del"></button>
+    </td>
+  `
+  );
+
+  return tr;
+};
